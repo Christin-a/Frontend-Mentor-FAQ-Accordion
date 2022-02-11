c14b502ce0a5d96fe599e999c7eaa676d0abb488
Christina Armand <christina.m.armand@gmail.com>
2022-02-11 00:19:50 -0500

    Finished Desktop v1, Mobile Needs Work

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 var itemHeader = document.querySelectorAll(".item-header");
 
 itemHeader.forEach(itemHeader => {
-itemHeader.addEventListener("click", event => {
-    itemHeader.classList.toggle("active");
+    itemHeader.addEventListener("click", event => {
+        itemHeader.classList.toggle("active");
 
-var itemBody = itemHeader.nextElementSibling;
-if(itemHeader.classList.contains("active")) {
-    itemBody.style.maxHeight = itemBody.scrollHeight + "px";
-    itemBody.style.paddingBottom = "20px";
-}else{
-    itemBody.style.maxHeight = 0;
-}
-    
-});
+        var itemBody = itemHeader.nextElementSibling;
+        if (itemHeader.classList.contains("active")) {
+            itemBody.style.maxHeight = itemBody.scrollHeight + "px";
+        } else {
+            itemBody.style.maxHeight = 0;
+
+        }
+
+    });
 });
 
+
+Ï
